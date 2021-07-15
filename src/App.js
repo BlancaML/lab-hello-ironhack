3795f12bd3dc6129f7a78deb113601083ec1dff5
BlancaML <blancamorenodeluna@gmail.com>
2021-07-15 21:41:31 +0200

    feat: middle section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
+import logo from './ironhack-logo.svg';
 import './App.css';
+import Button from './Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+
+
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Say Hello to React JS</h1>
+        <p>You will learn how to use the most popular frontend library and become a super Ninja 
+          Developer.</p>
+          <Button text="Awesome"/>
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Button.css'
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button className="Button">Awesome!</button>
+        )
+    }
+}
+
+export default Button
